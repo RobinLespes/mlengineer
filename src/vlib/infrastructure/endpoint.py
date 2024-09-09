b12cd8b8300efcdf87623a1da0197d03f8fe1bae
rlespes <rlespes@veepee.com>
2024-09-09 23:29:19 +0200

    refacto serving

--- src/vlib/infrastructure/endpoint.py
+++ src/vlib/infrastructure/endpoint.py
@@ -6,25 +6,14 @@ model_name = "vlib_rf"
 model_version = "2"
 model = mlflow.pyfunc.load_model(f"models:/{model_name}/{model_version}")
 app = Flask(__name__)
-
-
 @app.route('/predict', methods=['POST'])
 def predict():
     if request.method == 'POST':
         try:
-            # Get the data from the request
             data = request.get_json()
-
-            # Convert the data into a pandas DataFrame
             df = pd.DataFrame(data)
-
-            # Generate predictions
             predictions = model.predict(df)
-
-            # Convert predictions to a list
             result = predictions.tolist()
-
-            # Respond with the predictions
             return jsonify({'predictions': result})
 
         except Exception as e:
